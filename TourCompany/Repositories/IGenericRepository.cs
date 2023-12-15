9605bf2b4499f0ce1bf457ee2e073f8cf165dc56
OleksandrYezhov19 <OleksandrYezhov19@gmail.com>
2023-12-15 21:43:22 +0200

    Creating Repository layer

---
+++ TourCompany/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourCompany.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        DbSet<TEntity> GetEntity();
+        Task AddAsync(TEntity entity);
+        Task DeleteByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int? id);
+        Task UpdateAsync(TEntity entity);
+    }
+}
---
+++ TourCompany/Repositories/impl/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TourCompany.Repositories.impl
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context, DbSet<TEntity> dbSet)
+        {
+            _context = context;
+            _dbSet = dbSet;
+        }
+
+        public DbSet<TEntity> GetEntity()
+        {
+            return _dbSet;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            TEntity? entityForRemoval = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entityForRemoval);
+            Save();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int? id)
+        {
+            TEntity? entity = await _dbSet.FindAsync(id);
+            return entity;
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+             _dbSet.Update(entity);
+            Save();
+        }
+
+        private void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
