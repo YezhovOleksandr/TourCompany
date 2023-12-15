cb27a117d7ac8b55630629bdf205621f5ecfb16b
OleksandrYezhov19 <OleksandrYezhov19@gmail.com>
2023-12-15 22:02:51 +0200

    Creating Service layer

---
+++ TourCompany/Servises/ITourService.cs
@@ -0,0 +1,13 @@
+﻿using Models.Models;
+
+namespace TourCompany.Servises
+{
+    public interface ITourService
+    {
+        Task<IEnumerable<Tour>> GetAllToursAsync();
+        Task AddTourAsync(Tour tour);
+        Task<Tour> GetTourByIdAsync(int? tourId);
+        Task UpdateTourAsync(Tour tour);
+        Task DeleteTourByIdAsync(int id);
+    }
+}
---
+++ TourCompany/Servises/impl/TourService.cs
@@ -0,0 +1,47 @@
+﻿using Models.Models;
+using TourCompany.Repositories;
+
+namespace TourCompany.Servises.impl
+{
+    public class TourService : ITourService
+    {
+        private readonly IGenericRepository<Tour> _tourRepository;
+
+        public TourService(IGenericRepository<Tour> tourRepository)
+        {
+            _tourRepository = tourRepository;
+        }
+        public async Task AddTourAsync(Tour tour)
+        {
+            await _tourRepository.AddAsync(tour);
+        }
+
+        public async Task DeleteTourByIdAsync(int id)
+        {
+            await _tourRepository.DeleteByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Tour>> GetAllToursAsync()
+        {
+            IEnumerable<Tour> tours = await _tourRepository.GetAllAsync();
+            return tours;
+        }
+
+        public async Task<Tour> GetTourByIdAsync(int? tourId)
+        {
+            Tour tour = await _tourRepository.GetByIdAsync(tourId);
+            return tour;
+        }
+
+        public async Task UpdateTourAsync(Tour tour)
+        {
+            Tour updateTour = await _tourRepository.GetByIdAsync(tour.TourId);
+            updateTour.TourName = tour.TourName;
+            updateTour.TourShortDescription = tour.TourShortDescription;
+            updateTour.TourDescription = tour.TourDescription;
+            updateTour.TourPrice = tour.TourPrice;
+
+            await _tourRepository.UpdateAsync(updateTour);
+        }
+    }
+}
